e1020cda68c1bf0ae9045c07b717301746f84d5c
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2015-07-27 11:01:55 -0700

    port change

--- FTPService/ftpserver.py
+++ FTPService/ftpserver.py
@@ -11,7 +11,7 @@ from twisted.internet import reactor
 p = Portal(FTPRealm('./', userHome='/home' ), [AllowAnonymousAccess(), FilePasswordDB("FTPService/key.dat")])
 f = FTPFactory(p)
 try:
-    reactor.listenTCP(2221, f)
+    reactor.listenTCP(21, f)
     reactor.run()
 except Exception as e:
     print "Error %s"%e
